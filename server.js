c715ceafeca8995bcc94e54370c713bf45915c26
Emma Duffy <emma.duffy@mycit.ie>
2024-11-01 22:45:18 +0000

    implement links

--- server.js
+++ server.js
@@ -11,13 +11,20 @@ mongoose.connect('mongodb://localhost/url-shortener', {
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
 })
 
-app.post('shortUrl', async (req, res) => {
+app.post('/shortUrls', async (req, res) => {
     await ShortUrl.create({ full: req.body.fullUrl });
     res.redirect('/');
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+    if (shortUrl == null) return res.sendStatus(404);
+    res.redirect(shortUrl.full);
 })
 
 app.listen(process.env.PORT || 5000);
